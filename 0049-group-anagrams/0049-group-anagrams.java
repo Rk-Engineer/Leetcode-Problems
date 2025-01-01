bd34e7659e4c3e4c8fb53d9b2128de2cd0ef2870
Rk-Engineer <150328082+Rk-Engineer@users.noreply.github.com>
2025-01-01 10:14:41 +0530

    Time: 20 ms (16.01%), Space: 48.3 MB (25.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,18 +2,22 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map = new HashMap<>();
         for(String s : strs){
-            int [] b = new int[26];
+            //frequency array 
+            int[] b = new int[26];
+            //add all chars in frequency array 
             for(char num : s.toCharArray()){
-                b[num - 'a']++;
+                b[num-'a']++ ;
             }
+            // create a stringbuilder to store the frequencies
             StringBuilder sb = new StringBuilder();
             for(int c : b){
-                sb.append(c).append('-');
+                sb.append(c).append("-");
             }
+            // convert to string 
             String ans = sb.toString();
-            if(!map.containsKey(ans)){
-                map.put(ans,new ArrayList<>());
-            }
+            // save the key in hash map , if the key is not found 
+            map.putIfAbsent(ans,new ArrayList<>());
+            // get the key and save the string
             map.get(ans).add(s);
         }
         return new ArrayList<>(map.values());
