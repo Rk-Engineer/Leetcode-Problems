9606a7acea4bad04d3039367893c98067f45a25d
Ramkumar.B <150328082+Rk-Engineer@users.noreply.github.com>
2025-06-05 20:52:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1508-longest-happy-prefix/1508-longest-happy-prefix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int m = s.length();   // use --> KMP Algorithm //
+
+        // step 1 : create an empty LPS array 
+        int[] lps = new int[m];
+
+        // step 2 : prepare the LPS array
+        int i = 1 , prev = 0 ;
+        while(i < m){
+            // case 1 : character at i and prev pointer matches  
+            if(s.charAt(i) == s.charAt(prev)){
+                lps[i] = prev + 1 ;
+                i++ ;
+                prev++ ;
+            }
+            // case 2 : if character does not matches
+            else{
+                if( prev == 0 ){
+                    lps[i] = 0 ;
+                    i++ ;
+                }
+                else{
+                    prev = lps[prev] - 1 ;
+                }
+            } 
+            
+        }
+        int last = lps[lps.length - 1];
+        return s.substring(0,last);
+    }
+}
