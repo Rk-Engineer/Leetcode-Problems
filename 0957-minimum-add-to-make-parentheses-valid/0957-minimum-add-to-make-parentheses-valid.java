ba5c0f545cc601049a6f69d7ec7853eb5eee5b81
Ramkumar.B <150328082+Rk-Engineer@users.noreply.github.com>
2025-06-08 10:34:57 +0530

    Time: 2 ms (23.97%), Space: 42 MB (8.54%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,28 +1,17 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Integer> open = new Stack<>();
-        Stack<Integer> close = new Stack<>();
-        int count = 0 ;
-        for(int i = 0 ; i < s.length() ; i++){
+        Stack<Character> brac = new Stack<>();
+        for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
-            // Case 1 : Open Brackets
-            if(ch == '(') open.push(i);
-            // Case 2 : Close Brackets
+            if(ch ==')'){
+                if(brac.isEmpty()) brac.add(ch);
+                else if(brac.peek()  == ch ) brac.add(ch);
+                else brac.pop();
+            }
             else{
-                if(!open.isEmpty()) open.pop();
-                else{
-                    close.push(i);
-                }
+                brac.add(ch);
             }
         }
-        while(!open.isEmpty() && !close.isEmpty()){
-            if(open.pop()>close.pop()) count+=2 ;
-        }
-        while(!open.isEmpty()){
-            open.pop();count++;
-        }while(!close.isEmpty()){
-            close.pop();count++;
-        }
-        return count ;
+        return brac.size();
     }
 }
