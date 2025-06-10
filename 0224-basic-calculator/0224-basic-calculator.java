20bfd013bdac53878fda7d8d4a56a212e58114fb
Ramkumar.B <150328082+Rk-Engineer@users.noreply.github.com>
2025-06-10 08:02:52 +0530

    Time: 7 ms (85.05%), Space: 44.5 MB (39.3%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,39 +1,38 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
-        int num =0 , res =0 , sign = 1 ; 
-        for(int i = 0 ;i< s.length();i++){
-            char ch = s.charAt(i);
+        int num = 0 , sign = 1 , res  = 0 ;
+        for(char ch : s.toCharArray()){
+            // CASE 1 : character is Digit
             if(Character.isDigit(ch)){
-                num = num * 10 + ch-'0' ;
+                num = num*10 + (ch - '0');
             }
-            else if (ch == '+'){
-                res+= num* sign ;
-
-                num =0 ;
+            else if(ch == '+'){
+                res =res + (num*sign) ;
+                num = 0 ;
                 sign = 1 ;
             }
-            else if (ch == '-'){
-                res+= num* sign ;
-
-                num =0 ;
+            else if(ch == '-'){
+                res =res + (num*sign) ;
+                num = 0 ;
                 sign = -1 ;
             }
-            else if (ch == '('){
+            else if(ch == '('){
                 stack.add(res);
                 stack.add(sign);
 
-                res =0 ;
+                res = 0 ;
                 sign = 1 ;
             }
-            else if (ch == ')'){
-                res+= num* sign ;
-                res*= stack.pop();
-                res+= stack.pop();
-                num =0 ;
+            else if(ch == ')'){
+                res = res + (num*sign);
+                res = res * stack.pop();
+                res = res + stack.pop();
+
+                num = 0 ;
             }
         }
-        res += (num *sign) ;
+        res = res + (num*sign);
         return res ;
     }
 }
