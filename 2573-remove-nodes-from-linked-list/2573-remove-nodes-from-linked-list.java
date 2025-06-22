c785b9a2eb16a97fc594d55fd2d3c4462c16c6f7
Ramkumar.B <150328082+Rk-Engineer@users.noreply.github.com>
2025-06-22 20:24:00 +0530

    Time: 75 ms (9.88%), Space: 61.6 MB (97.13%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.java
@@ -1,29 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        head =  reverse(head);
-        ListNode curr = head;
-
-        while (curr.next != null){
-            if (curr.next.val >= curr.val){
-                curr = curr.next;
+        Stack<ListNode> stack = new Stack<>();
+        ListNode curr = head ;
+        while(curr != null){
+            // Step 1 : check right side value is greater 
+            while(!stack.isEmpty() && stack.peek().val < curr.val){
+                stack.pop();
             }
-            else curr.next = curr.next.next;
+            stack.add(curr);
+            curr = curr.next ;
         }
-
-        return reverse(head);
-    }
-
-    public ListNode reverse(ListNode head){
-        ListNode prev = null;
-
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
+        // Step 2 : Create a new RESULT Linked List
+        ListNode newHead = null ;
+        while (!stack.isEmpty()){
+            ListNode temp = stack.pop();
+            temp.next = newHead ;
+            newHead = temp ;
         }
-
-        return prev;
+        return newHead ;
     }
 }
-
